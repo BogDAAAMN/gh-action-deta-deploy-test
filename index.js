37183bca61059317e6335469d056a72e8439ff2f
Bogdan Covrig <bogcov@gmail.com>
2020-12-07 23:50:34 +0100

    Update index.js

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ const express = require('express');
 const app = express(); // instantiate express
 
 app.get('/hey', async (req, res) => {
-   res.send('hey gh again!!!')
+   res.send('hey gh again!!! from actions this time <3')
 });
 
 module.exports = app;
